69142c994a5bafec5d53eb2bcbd916b91ab540d2
Luis-unk <santosluiss2022@gmail.com>
2024-08-05 20:51:32 -0300

    react-native class - Counter

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
+  const handlerSum = () => setCount(count + 1);
+  const clearSum = () => (setCount(0), Alert.alert("Tudo Limpo!"));
+  const handlerSub = () => setCount(count - 1);
+  const handlerText = (text) => setText(text);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Contador</Text>
+      <Text style={styles.textStyle}>O valor do contador é: {count}</Text>
+      <View style={styles.buttonList}>
+        <Button title="Somar" onPress={handlerSum} color={"green"}></Button>
+        <Button
+          title="Subtrair"
+          onPress={handlerSub}
+          color={"#fbf451"}
+        ></Button>
+        <Button title="Limpar" onPress={clearSum} color={"red"}></Button>
+      </View>
+      <TextInput
+        placeholder="Digite aqui"
+        style={styles.inputText}
+        onChangeText={handlerText}
+      ></TextInput>
+      <Text style={styles.textStyle}>O texto digitado é: {text}</Text>
     </View>
   );
 }
@@ -13,8 +44,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  title: {
+    fontSize: 50,
+    margin: 10,
+    alignItems: "center",
+  },
+
+  textStyle: {
+    marginTop: 10,
+    fontSize: 20,
+  },
+
+  inputText: {
+    height: 40,
+    borderColor: "gray",
+    borderRadius: 6,
+    borderWidth: 1,
+    marginTop: 30,
+    padding: 10,
+    width: "80%",
+  },
+
+  buttonList: {
+    width: 300,
+    display: "flex",
+    marginTop: 20,
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+
+  buttonDesign: {
+    borderRadius: 6,
+    fontSize: 10,
+    height: 40,
   },
 });
